9a5df7080383f73f0975ce45068b4474799eb8c6
Tanishq123-Gamer <62173728+Tanishq123-Gamer@users.noreply.github.com>
2024-01-29 22:53:24 -0600

    Add files via upload

---
+++ WardrobeManager.java
@@ -0,0 +1,37 @@
+public class WardrobeManager {
+    public static boolean containsClothing(String description, String
+            brand, String[][] wardrobe, int wardrobeSize) {
+    }
+
+    public static int addClothing(String description, String brand,
+                                  String[][] wardrobe, int wardrobeSize) {
+    }
+
+    public static int indexOfClothing(String description, String brand,
+                                      String[][] wardrobe, int wardrobeSize) {
+    }
+
+    public static boolean wearClothing(String description, String brand,
+                                       String date, String[][] wardrobe, int wardrobeSize) {
+    }
+
+    public static int getBrandCount(String brand, String[][] wardrobe, int
+            wardrobeSize) {
+    }
+
+    public static int getNumUnwornClothes(String[][] wardrobe, int
+            wardrobeSize) {
+    }
+
+    public static int getMostRecentlyWorn(String[][] wardrobe, int
+            wardrobeSize) {
+    }
+
+    public static int removeClothingAtIndex(int index, String[][] wardrobe,
+                                            int wardrobeSize) {
+    }
+
+    public static int removeAllUnworn(String[][] wardrobe, int
+            wardrobeSize) {
+    }
+}
